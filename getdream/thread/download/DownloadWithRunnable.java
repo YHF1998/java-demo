9d3f00d274cff9a0a7a7fb66bca3604e9109e3d7
fenno <714326675@qq.com>
2025-01-17 11:17:34 +0800

    错误调整

--- getdream/thread/download/DownloadWithRunnable.java
+++ getdream/thread/download/DownloadWithRunnable.java
@@ -5,6 +5,11 @@ public class DownloadWithRunnable implements Runnable {
     private String url;//网络图片地址
     private String name;//图片保存名
 
+    public DownloadWithRunnable(String url, String name) {
+        this.name = name;
+        this.url = url;
+    }
+
     @Override
     public void run() {
         WebDownloader webDownloader = new WebDownloader();
@@ -16,9 +21,9 @@ public class DownloadWithRunnable implements Runnable {
     public static void main(String[] args) {
         //注意这里 继承Thread的话，在实例化后就能直接start()启动线程的
         //但是通过实现Runnable接口方式，是需要使用 new Thread（实例）.start()的方式进行调用并启动的
-        Download t1 = new Download("https://img2.baidu.com/it/u=2859542338,3761174075&fm=253&fmt=auto&app=138&f=JPEG?w=501&h=500", "1.jpg");
-        Download t2 = new Download("https://img2.baidu.com/it/u=2859542338,3761174075&fm=253&fmt=auto&app=138&f=JPEG?w=501&h=500", "2.jpg");
-        Download t3 = new Download("https://img2.baidu.com/it/u=2859542338,3761174075&fm=253&fmt=auto&app=138&f=JPEG?w=501&h=500", "3.jpg");
+        DownloadWithRunnable t1 = new DownloadWithRunnable("https://img2.baidu.com/it/u=2859542338,3761174075&fm=253&fmt=auto&app=138&f=JPEG?w=501&h=500", "1.jpg");
+        DownloadWithRunnable t2 = new DownloadWithRunnable("https://img2.baidu.com/it/u=2859542338,3761174075&fm=253&fmt=auto&app=138&f=JPEG?w=501&h=500", "2.jpg");
+        DownloadWithRunnable t3 = new DownloadWithRunnable("https://img2.baidu.com/it/u=2859542338,3761174075&fm=253&fmt=auto&app=138&f=JPEG?w=501&h=500", "3.jpg");
 
         //启动线程
         new Thread(t1).start();
